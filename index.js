a05294bcae573ec14457b9d9b4623965e2ca15af
EA - Fredson Vicente Barbosa Junior <fredson_vicente@educadventista.org>
2023-10-31 13:50:10 -0300

    recebendo parâmetros na rota

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+// https://localhost:3000/usuarios
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados do usuário ${id}`)
+
+    resposta.sendFile(`${caminhobase}/usuarios.html`)
+})
+
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
 })
